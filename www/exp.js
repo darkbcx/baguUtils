f40838fab37a7d1a955399c899eb54998577a087
darkbcx <darkbcx@yahoo.com>
2015-10-09 20:39:16 +0700

    second commit

---
+++ www/exp.js
@@ -0,0 +1,16 @@
+angular.module('expApp', ['ngMaterial', 'ngMessages', 'ngMdIcons', 'baguUtils'])
+    .controller('expCtl', ['$scope', 'baguAuth0Service', 'baguCommonService', expCtl])
+;
+
+function expCtl($scope, baguAuth0Service, baguCommonService) {
+    var self = this;
+
+    // -- Properties init
+
+    // -- Methods
+
+    //====================================================================================================
+    // -- Constructor.. kind of
+
+}
+
